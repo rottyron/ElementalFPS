9258e7b6ff579de2b92d416818adf7da0386f855
JamesK <rottyron@gmail.com>
2024-04-03 17:50:22 +1300

    Initial Commit
    Initial commit

---
+++ Source/ElementalFPS/ElementalFPSGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ElementalFPSGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AElementalFPSGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AElementalFPSGameMode();
+};
+
+
+
